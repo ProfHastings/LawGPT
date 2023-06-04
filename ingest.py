8c07f427d2448e1ddb63faefcbb1bde952331e3b
ProfHastings <philipzeiler@icloud.com>
2023-06-05 00:43:34 +0200

    ingest probably working now

--- ingest.py
+++ ingest.py
@@ -1,25 +1,38 @@
 from pathlib import Path
-from langchain.text_splitter import CharacterTextSplitter
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import FAISS
 from sentence_transformers import SentenceTransformer
-import pickle
 import torch
 import time
 import numpy as np
 from pinecone_text.sparse import BM25Encoder
 import pinecone
+import itertools
 
-# Here we load in the data in the format that a directory of .txt files is in.
+print(torch.__version__)
+print(torch.cuda.is_available())
+print(torch.backends.cudnn.version())
+
+
+def chunks(iterable, batch_size=100):
+    it = iter(iterable)
+    chunk = tuple(itertools.islice(it, batch_size))
+    while chunk:
+        yield chunk
+        chunk = tuple(itertools.islice(it, batch_size))
+
 ps = list(Path("GmbHG/").glob("**/*.txt"))
-
-# Initialize the SentenceTransformer model
 model_name = 'T-Systems-onsite/cross-en-de-roberta-sentence-transformer'
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = SentenceTransformer(model_name)
+model = model.to(device) # Move the model to the GPU
 
 bm25_encoder = BM25Encoder()
 
-index = pinecone.Index("justiz-9594bd4.svc.us-east-1-aws.pinecone.io")
+api_key = "2c3790ff-1d6a-48be-b101-1301723b6252"
+env = "us-east-1-aws"
+
+pinecone.init(api_key=api_key, environment=env)
+index = pinecone.Index("justiz")
 
 data = []
 sources = []
@@ -29,47 +42,50 @@ for i, p in enumerate(ps):
     with open(p, 'rb') as f:
         data.append(f.read().decode('utf-8', errors='ignore'))    
     sources.append(p.stem)
-    if (i + 1) % 1000 == 0: # print every 1000 files
+    if (i + 1) % 1000 == 0: 
         elapsed_time = time.time() - start_time
         print(f"Processed {i+1} files in {elapsed_time:.2f} seconds.")
 
-
-# Here we split the documents, as needed, into smaller chunks.
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1500)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000)
 docs = []
 metadatas = []
 for i, d in enumerate(data):
     splits = text_splitter.split_text(d)
     docs.extend(splits)
-    metadatas.extend([{"source": sources[i]}] * len(splits))
+    metadatas.extend([{"source": f"{sources[i]}_{j}"} for j in range(len(splits))])  # attach unique index to source
 
 print("fitting bm25_encoder...")
 bm25_encoder.fit(docs)
 bm25_encoder.dump("bm25_values.json")
+#bm25_encoder = BM25Encoder().load("bm25_values.json")
+bm25_embeddings = bm25_encoder.encode_documents(docs)
 
-# Generate document embeddings
 print("Generating embeddings...")
 start_time = time.time()
 embeddings = []
-batch_size = 1000  # You may need to adjust this depending on your GPU's memory capacity
+batch_size = 1000
 for i in range(0, len(docs), batch_size):
     batch = docs[i:i+batch_size]
-    embeddings.extend(model(batch))
+   # batch = [t.to(device) for t in batch] # Move the data to the GPU
+    embeddings.extend(model.encode(batch, convert_to_tensor=True))
+
     print(i)
 elapsed_time = time.time() - start_time
 print(f"Generated embeddings for {len(docs)} documents in {elapsed_time:.2f} seconds.")
 
 print(len(embeddings))
 
-# Create a vector store from the embeddings and save it to disk
-print("Creating vector store...")
-start_time = time.time()
-embeddings = list(zip(docs, embeddings))
-#store = FAISS.from_embeddings(embeddings, model, metadatas=metadatas)
-#faiss.write_index(store.index, "docs.index")
-elapsed_time = time.time() - start_time
-print(f"Created vector store in {elapsed_time:.2f} seconds.")
+dense_embeddings = [emb.tolist() for emb in embeddings]
 
-# Save the entire store
-#with open("store.pkl", "wb") as f:
-#    torch.save(store, f, pickle_protocol=4)
+# Assuming bm25_embeddings is a list of scipy csr_matrix
+sparse_values = [{"indices": emb["indices"], "values": emb["values"]} for emb in bm25_embeddings]
+
+# Prepare the data for upsertion to Pinecone index
+data_to_upsert = [{"id": f"{metadata['source']}_{i}", "values": dense_emb, "metadata": metadata, "sparse_values": sparse_emb} 
+                  for i, (doc, dense_emb, sparse_emb, metadata) in enumerate(zip(docs, dense_embeddings, sparse_values, metadatas))]
+
+# Upsert data into Pinecone index in chunks
+for batch in chunks(data_to_upsert, batch_size=100):
+    upsert_response = index.upsert(vectors=batch)
+    if upsert_response.error:
+        print(f'Error during upsert: {upsert_response.error}')
