e8357d373c3e9b7dd1a0426d3fa1545db5e1a4b4
Philip Zeiler <104988606+ProfHastings@users.noreply.github.com>
2023-06-08 23:52:17 +0200

    moved everything to functions

--- api_test.py
+++ api_test.py
@@ -14,66 +14,76 @@ from langchain.schema import (
 )
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
-
-os.environ["TOKENIZERS_PARALLELISM"] = "false"
-# Pinecone
-api_key = "2c3790ff-1d6a-48be-b101-1301723b6252"
-env = "us-east-1-aws"
-pinecone.init(api_key=api_key, environment=env)
-index = pinecone.Index("justiz")
-
-# Loading models
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-splade = SpladeEncoder(device=device)
-model_name = 'T-Systems-onsite/cross-en-de-roberta-sentence-transformer'
-embeddings = HuggingFaceEmbeddings(model_name=model_name)
+#init of global gpt4 model and OpenAI tokenizer
 callback_handler = [StreamingStdOutCallbackHandler()]
 gpt4 = ChatOpenAI(model_name="gpt-4", temperature=0, max_tokens=2048, streaming=True, callbacks=callback_handler, openai_api_key="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")
+tokenizer = tiktoken.encoding_for_model("gpt-4")
 
-retriever = PineconeHybridSearchRetriever(embeddings=embeddings, sparse_encoder=splade, index=index, top_k=50, alpha=0.3) #lower alpha - more sparse
+#init of prompt templates and system message
+system_message = SystemMessage(content="Du bist ein erfahrener Anwalt mit dem Spezialgebiet österreichisches Recht.")
 
-question = "Fred arbeitet für Max in einem Tonstudio. Fred macht eine tolle Erfindung, die es ihm ermöglicht auf elektronischem Weg Pfurtzgeräusche zu erzeugen. Zu einem geringen Teil hat er an dieser Erfindung während seiner Arbeitszeit gearbeitet. Wem gehört die Erfindung?"
-results = retriever.get_relevant_documents(question)
-
-tokenizer = tiktoken.encoding_for_model("gpt-4") 
-
-template = """
+analysis_template_string = """
 Deine Aufgabe ist es die folgende Frage zu beantworten: "{question}"
 Um die Frage zu beantworten hast du die folgenden Entscheidungen des Österreichischen Obersten Gerichtshofes zur Verfügung:
 "{sources}"
 Schreib eine ausführliche legale Analyse der Frage im Stil eines Rechtsgutachtens und gib für jede Aussage die entsprechende Quelle in Klammer an.
 Beschreibe die Rechtsfrage abstrakt und ergänze deine Ausführungen mit praktischen Besipielen, die du in den Entscheidungen des Obersten Gerichtshofs findest. 
-Vergleiche diese Besipiele auch mit dem Fall der der Frage zugrunde liegt.
+Vergleiche diese Beispiele auch mit dem Fall der der Frage zugrunde liegt.
 """
+analysis_template = PromptTemplate.from_template(analysis_template_string)
 
-token_count = len(list(tokenizer.encode(template))) + len(list(tokenizer.encode(question)))
-max_token_limit = 6100
-#6144
-#lower bc system
-source_info = ""
-nrsources = 0 
-for result in results:
-    new_text = f"Inhalt: {result.page_content}\nQuelle: {result.metadata['source']}\n"
-    new_tokens = list(tokenizer.encode(new_text))
-    new_token_count = len(new_tokens)
+#takes Vector Database results, returns highest number of results with sources as string that fit in max_tokens OpenAI
+def fill_tokens(results, max_tokens):
+    sources = ""
+    nr_sources = 0
+    token_count = 0
+    
+    for result in results:
+        new_text = f"Inhalt: {result.page_content}\nQuelle: {result.metadata['source']}\n"
+        new_tokens = list(tokenizer.encode(new_text))
+        new_token_count = len(new_tokens)
+        if token_count + new_token_count > max_tokens:
+            break
+        sources += new_text
+        token_count += new_token_count
+        nr_sources += 1
+    print(f"Used {nr_sources} sources")
+    return sources
 
-    if token_count + new_token_count > max_token_limit:
-        break
+#initializes Pinecone index to make database requests
+def get_index():
+    api_key = "2c3790ff-1d6a-48be-b101-1301723b6252"
+    env = "us-east-1-aws"
+    pinecone.init(api_key=api_key, environment=env)
+    index = pinecone.Index("justiz")
+    return index
 
-    source_info += new_text
-    token_count += new_token_count
-    nrsources += 1
+#loads dense and sparse encoder models and 
+def get_retriever():
+    index = get_index
+    os.environ["TOKENIZERS_PARALLELISM"] = "false"
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model_name = 'T-Systems-onsite/cross-en-de-roberta-sentence-transformer'
+    dense_encoder = HuggingFaceEmbeddings(model_name=model_name)
+    sparse_encoder = SpladeEncoder(device=device)
+    retriever = PineconeHybridSearchRetriever(embeddings=dense_encoder, sparse_encoder=sparse_encoder, index=index, top_k=50, alpha=0.3) #lower alpha - more sparse
+    return retriever
 
-print(f"Used {nrsources} sources")
-prompt_template = PromptTemplate.from_template(template)
+def main():
+    retriever = get_retriever
 
-gpt4userprompt = prompt_template.format(question=question, sources=source_info)
-#print(gpt4userprompt)
+    question = "Fred arbeitet für Max in einem Tonstudio. Fred macht eine tolle Erfindung, die es ihm ermöglicht auf elektronischem Weg Pfurtzgeräusche zu erzeugen. Zu einem geringen Teil hat er an dieser Erfindung während seiner Arbeitszeit gearbeitet. Wem gehört die Erfindung?"
+    results = retriever.get_relevant_documents(question)
+    max_tokens = ((8192 - 2048) - 20) - len(list(tokenizer.encode(analysis_template_string))) + len(list(tokenizer.encode(question)))
+    sources = fill_tokens(results=results, max_tokens=max_tokens)
 
-system_message = SystemMessage(content="Du bist ein erfahrener Anwalt mit dem Spezialgebiet österreichisches Recht.")
+    analysisuserprompt = analysis_template.format(question=question, sources=sources)
+    #print(gpt4userprompt)
 
-user_message = HumanMessage(content=gpt4userprompt)
+    user_message = HumanMessage(content=analysisuserprompt)
 
-for response in gpt4([system_message, user_message]):
-    print(response.content)
+    for response in gpt4([system_message, user_message]):
+        print(response.content)
 
+if __name__ == "__main__":
+    main()
