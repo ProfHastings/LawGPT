f8bcae5e06c6ea5b00c24ea6740bf70088e5bb4b
Philip Zeiler <104988606+ProfHastings@users.noreply.github.com>
2023-06-12 15:14:38 +0200

    streamlit fixed

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,16 @@
 import streamlit as st
 from api_test_openai import main
 
+st.set_page_config(page_title="zeilertech", page_icon=":scales:")
+
 st.title("Legal Chatbot")
 question = st.text_input("Please enter your legal question here:")
 
-st.set_page_config(page_title="zeilertech", page_icon=":fountain_pen:")
-
 if st.button("Submit"):
-    response = main(question)  # Assuming your main function accepts the question as an argument and returns the response
-    st.markdown(f"**Response:**\n{response}")
+    with st.spinner('Konzipienten werden gepeitscht...'):
+        try:
+            response = main(question)
+        except Exception as e:
+            st.error(f"Konzipient braucht mokka latte mit pumpkin spice (no gluten) weil {str(e)}")
+        else:
+            st.text_area("Rechtsgutachten:", response)
